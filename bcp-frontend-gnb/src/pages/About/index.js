9a2b318e31a0e36bf736edaa62cecb4df5ba659a
Dominic <xodnghdud109@gmail.com>
2023-10-20 16:27:24 +0900

    안쓰는 폴더 삭제

--- bcp-frontend-gnb/src/pages/About/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const About = () => {
-  return <h1 style={{ background: "pink" }}>About</h1>;
-};
-export default About;
--- bcp-frontend-gnb/src/pages/Dashboard/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Dashboard = () => {
-  return <h1 style={{ background: "pink" }}>Dashboard</h1>;
-};
-export default Dashboard;
